0cf4f017073826d0317d8664c1e61a6561477e30
Leander Balmeo <leanderbalmeo@rocketmail.com>
2021-10-27 12:30:45 +0200

    Add files via upload

--- src/main/java/com/example/calculator308/Calculator.java
+++ src/main/java/com/example/calculator308/Calculator.java
@@ -11,9 +11,25 @@ import javafx.stage.Stage;
 
 public class Calculator extends Application {
 
-    Label Label1;
-    Button butt0,butt1,butt2,butt3,butt4,butt5,butt6,butt7,butt8,butt9,buttadd,buttsub,buttmult,buttdiv,butteq;
-    String i1, i2, op;
+    Label label1;
+    Button butt0;
+    Button butt1;
+    Button butt2;
+    Button butt3;
+    Button butt4;
+    Button butt5;
+    Button butt6;
+    Button butt7;
+    Button butt8;
+    Button butt9;
+    Button buttadd;
+    Button buttsub;
+    Button buttmult;
+    Button buttdiv;
+    Button butteq;
+    String i1;
+    String i2;
+    String op;
 
     public static void main(String[] args) {
         launch(args);
@@ -22,8 +38,7 @@ public class Calculator extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Very Crude Calculator");
-        Label1 = new Label("Equation will display here.");
-        System.out.println("Answers will display under me.");
+        label1 = new Label("Equation will display here.");
         VBox root = new VBox();
         Scene scene = new Scene(root, 400, 800);
         stage.setScene(scene);
@@ -43,9 +58,9 @@ public class Calculator extends Application {
         buttsub = makeButton("-");
         buttmult = makeButton("*");
         buttdiv = makeButton("/");
-        butteq = makeButton("=");
+        butteq = makeEqButton("=");
 
-        root.getChildren().addAll(Label1,butt0,butt1,butt2,butt3,butt4,butt5,butt6,butt7,butt8,butt9,buttadd,buttsub,buttmult,buttdiv,butteq);
+        root.getChildren().addAll(label1,butt0,butt1,butt2,butt3,butt4,butt5,butt6,butt7,butt8,butt9,buttadd,buttsub,buttmult,buttdiv,butteq);
         stage.show();
     }
 
@@ -54,29 +69,38 @@ public class Calculator extends Application {
         butt.setOnAction(new EventHandler<ActionEvent>() {
 
             public void handle(ActionEvent event) {
-                if (str.compareTo("=") == 0){
-                    if(i1.compareTo("") == 0 || i2.compareTo("") == 0 || op.compareTo("") == 0){
-                        System.out.println("Invalid equation. Please try again.");
-                        return;
-                    }
-                    float x = Integer.parseInt(i1);
-                    float y = Integer.parseInt(i2);
-                    System.out.print(i1 + " " + op + " " + i2 + " = ");
-                    if(op.compareTo("+") == 0) System.out.println(x + y);
-                    else if(op.compareTo("-") == 0) System.out.println(x - y);
-                    else if(op.compareTo("*") == 0) System.out.println(x * y);
-                    else System.out.println(x / y);
-                    i1 = op = i2 = "";
-                }
-                else if(str.compareTo("+") == 0 || str.compareTo("-") == 0 || str.compareTo("*") == 0 || str.compareTo("/") == 0){
-                    op = new String(str);
+                if(str.compareTo("+") == 0 || str.compareTo("-") == 0 || str.compareTo("*") == 0 || str.compareTo("/") == 0){
+                    op = str;
                 }
                 else if(op.compareTo("") == 0) i1 += str;
                 else i2 += str;
 
-                Label1.setText(i1 + " " + op + " " + i2);
+                label1.setText(i1 + " " + op + " " + i2);
             }
         });
+        return butt;
+    }
+
+    public Button makeEqButton(String str){
+        Button butt = new Button(str);
+        butt.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                if(i1.compareTo("") == 0 || i2.compareTo("") == 0 || op.compareTo("") == 0){
+                    label1.setText("Invalid equation. Please try again.");
+                    return;
+                }
+                float x = Integer.parseInt(i1);
+                float y = Integer.parseInt(i2);
+                if(op.compareTo("+") == 0) label1.setText(i1 + " " + op + " " + i2 + " = " + (x + y));
+                else if(op.compareTo("-") == 0) label1.setText(i1 + " " + op + " " + i2 + " = " + (x - y));
+                else if(op.compareTo("*") == 0) label1.setText(i1 + " " + op + " " + i2 + " = " + (x * y));
+                else if(y == 0) label1.setText("Error: Division by zero");
+                else label1.setText(i1 + " " + op + " " + i2 + " = " + (x / y));
+                i1 = op = i2 = "";
+            }
+        });
+
         return butt;
     }
 }
